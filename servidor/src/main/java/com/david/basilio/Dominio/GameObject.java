635dddebbf2c6a23518aa4a8fe1fc1ed5388a649
David <davidrojo1997@gmail.com>
2021-03-30 00:54:28 +0200

    Primer acercamiento GameObject y enum Tipo

--- servidor/src/main/java/com/david/basilio/Dominio/GameObject.java
+++ servidor/src/main/java/com/david/basilio/Dominio/GameObject.java
@@ -1,4 +1,29 @@
 package com.david.basilio.Dominio;
 
 public abstract class GameObject {
+
+
+
+    protected final Tipo tipo;
+
+    protected int positionX, positionY;
+    protected float rotation;
+    protected GameObjectManagerUser gameObjectManagerUser;
+
+
+    public GameObject(Tipo tipo, int positionX, int positionY, float rotation, GameObjectManagerUser gameObjectManagerUser) {
+        this.tipo = tipo;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.rotation = rotation;
+        this.gameObjectManagerUser = gameObjectManagerUser;
+    }
+
+    public abstract void update();
+
+    public abstract void colliding();
+
+    public void die(){
+
+    }
 }
---
+++ servidor/src/main/java/com/david/basilio/Dominio/Tipo.java
@@ -0,0 +1,8 @@
+package com.david.basilio.Dominio;
+
+public enum Tipo {
+    CAJITA,
+    DISPARO,
+    BARCO,
+    ISLA
+}
