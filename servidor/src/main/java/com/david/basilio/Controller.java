2020f54700f59dee9bd6a7c60ae312f6c1ccea57
Basilio David Gomez Fernandez <basilio.david.g@gmail.com>
2021-04-04 19:40:21 +0200

    Quitando swing dependences

--- servidor/src/main/java/com/david/basilio/Controller.java
+++ servidor/src/main/java/com/david/basilio/Controller.java
@@ -1,9 +1,50 @@
 package com.david.basilio;
 
-import java.net.Socket;
+// File Name GreetingServer.java
+import java.net.*;
+import java.io.*;
 
-public class Controller {
+public class Controller extends Thread {
+	private ServerSocket serverSocket;
+
+	public Controller(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Waiting for client on port " +
+						serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+
+				System.out.println("Just connected to " + server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(server.getInputStream());
+
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(server.getOutputStream());
+				out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+						+ "\nGoodbye!");
+				server.close();
+
+			} catch (SocketTimeoutException s) {
+				System.out.println("Socket timed out!");
+				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-//		Socket socket = new Socket()
+		int port = Integer.parseInt(args[0]);
+		try {
+			Thread t = new Controller(port);
+			t.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
